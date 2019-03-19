6b8593253b95bc72a624409dafabe1b3c860d906
Mark Lokhorst <mlokhors@f1r6s5.codam.nl>
2019-03-19 19:42:41 +0100

    ex12v2

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: mlokhors <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/18 17:17:15 by mlokhors      #+#    #+#                 */
-/*   Updated: 2019/03/19 19:35:00 by mlokhors      ########   odam.nl         */
+/*   Updated: 2019/03/19 19:41:49 by mlokhors      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@ int	ft_iterative_factorial(int nb)
 	int total;
 
 	total = nb;
-	if (nb <= 0 || nb > 12)
+	if (nb < 0 || nb > 12)
 		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
 	while (nb > 1)
 	{
 		total = total * (nb - 1);
