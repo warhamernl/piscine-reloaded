420386aa47281bb4d048f2e00af01288b9e8300a
Mark Lokhorst <mlokhors@f1r6s5.codam.nl>
2019-03-19 19:56:03 +0100

    ex19

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_sort_params.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mlokhors <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/19 10:52:34 by mlokhors      #+#    #+#                 */
+/*   Updated: 2019/03/19 19:55:23 by mlokhors      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_swap(char **s1, char **s2)
+{
+	char *temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	print(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar("\n");
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (j + 1 < argc)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+				ft_swap(&argv[j], &argv[j + 1]);
+			j++;
+		}
+		i++;
+	}
+	print(argc, argv);
+	return (0);
+}
