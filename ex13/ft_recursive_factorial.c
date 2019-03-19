d3dbe8d5ec277c89b37c4f14c0c57002184f214a
Mark Lokhorst <mlokhors@f1r6s5.codam.nl>
2019-03-19 19:37:39 +0100

    ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_recursive_factorial.c                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mlokhors <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/18 18:18:26 by mlokhors      #+#    #+#                 */
+/*   Updated: 2019/03/19 19:37:04 by mlokhors      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_recursive_factorial(int nb)
+{
+	if (nb <= 0 || nb > 12)
+		return (0);
+	if (nb > 1)
+		return (nb * ft_recursive_factorial(nb - 1));
+	else
+		return (nb);
+}
