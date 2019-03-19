f2d1b4c4ce4496584ab6ccc29212b6f9f3ce05aa
Mark Lokhorst <mlokhors@f1r6s5.codam.nl>
2019-03-19 19:20:53 +0100

    ex07

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_print_numbers.c                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mlokhors <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/18 16:33:19 by mlokhors      #+#    #+#                 */
+/*   Updated: 2019/03/18 16:43:30 by mlokhors      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	char number;
+
+	number = '0';
+	while (number <= '9')
+		ft_putchar(number++);
+}
