535cdc8ac9f2b694315c9d224fc1df42dccb313a
Mark Lokhorst <mlokhors@f1r6s5.codam.nl>
2019-03-19 19:15:49 +0100

    ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_print_alphabet.c                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mlokhors <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/18 16:12:19 by mlokhors      #+#    #+#                 */
+/*   Updated: 2019/03/18 16:30:23 by mlokhors      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
